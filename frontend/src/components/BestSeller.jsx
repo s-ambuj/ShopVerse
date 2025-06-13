a14ad94b2fd62842ced9931f77e3cadfc8d3d0e5
Ambuj Singh <ambujsingh026@gmail.com>
2025-06-13 20:06:35 +0530

    updated BestSeller.jsx

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -17,7 +17,7 @@ const BestSeller = () => {
         <div className='text-center text-3xl py-8'>
             <Title text1={'BEST'} text2={'SELLERS'}/>
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, labore voluptatem. Nobis, tenetur. Veniam, a!</p>
+            Discover our best-selling products—tried, tested, and trusted by thousands of happy customers.</p>
         </div>
 
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
