fec7f1859201c80e0f2ac9131a386ccb929d2882
Ambuj Singh <ambujsingh026@gmail.com>
2025-06-13 20:06:50 +0530

    updated LatestCollection.jsx

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -20,7 +20,7 @@ const LatestCollection = () => {
         <div className='text-center py-8 text-3xl'>
             <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, voluptatum.</p>
+            Fresh drops just in! Get your hands on the trendiest products making waves this season.</p>
         </div>
         {/* Render Products */}
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
