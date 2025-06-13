ecb522d52e1f1e20a61d4772540bc2ee191aac7b
Ambuj Singh <ambujsingh026@gmail.com>
2025-06-13 20:06:05 +0530

    updated footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
     <div>
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         <div>
-            <img className='mb-5 w-32' src={assets.logo} alt='' />
+            <img className='w-32' src={assets.logo} alt='' />
             <p className='w-full md:w-2/3 text-gray-600'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id quidem, suscipit asperiores minima ex quasi ipsa dolorem et nemo mollitia.
+                Beyond Shopping, Into the Verse.
             </p>
         </div>
 
