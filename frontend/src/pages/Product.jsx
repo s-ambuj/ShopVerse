15c818e353ff5a473f334bf03c978913118ef8ee
Ambuj Singh <ambujsingh026@gmail.com>
2025-06-14 17:31:19 +0530

    create Product.jsx

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const Product = () => {
+
+  const { productID } = useParams();
+  console.log(productID);
   return (
     <div>
       
