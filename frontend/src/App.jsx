128c911cb399ecc6322175a06f9b5be2f58ebea7
Ambuj Singh <ambujsingh026@gmail.com>
2025-06-16 11:05:07 +0530

    error messages integration using react-toastify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/contact'
-import Product from './pages/product'
+import Product from './pages/Product'
 import Collection from './pages/collection'
 import Cart from './pages/Cart'
 import LoginPage from './pages/LoginPage'
@@ -12,11 +12,13 @@ import Orders from './pages/Orders'
 import Navbar from './components/navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
 
 
 const App = () => {
   return (
     <div className = 'px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer />
       <Navbar />
       <SearchBar />
       <Routes>
