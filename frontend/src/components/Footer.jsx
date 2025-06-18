41d59261a9c55a05528974249d092f04f94c19ae
Ambuj Singh <ambujsingh026@gmail.com>
2025-06-18 12:02:59 +0530

    created Contact.jsx page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
         <div>
             <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
             <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>+1 123-456-7890</li>
+                <li>9876543210</li>
                 <li>contact@shopverse.com</li>
             </ul>
         </div>
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/frontend_assets/assets'
+import NewsLetter from '../components/NewsLetter'
 
 const Contact = () => {
   return (
     <div>
       
+      <div className='text-center text-2xl pt-10 border-t'>
+        <Title text1={'CONTACT'} text2={'US'} />
+      </div>
+
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+        <img className='w-full md:max-w-[480px]' src={assets.contact_img} />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+          <p className='text-gray-500'>Vaishnavi Summit, Ground Floor, 7th Main, 80 Feet Road, 3rd Block, <br /> Koramangala Industrial Layout, Bangalore KA IN 560034</p>
+          <p className='text-gray-500'>Call: 9876543210 <br /> Email: contact@shopverse.com </p>
+          <p className='font-semibold text-xl text-gray-600'>Careers at ShopVerse</p>
+          <p className='text-gray-500'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, iusto.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+      </div>
+      <NewsLetter />
     </div>
   )
 }
