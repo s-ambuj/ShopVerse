bee367f499c8f441c915772bbea48206cdf79742
Ambuj Singh <ambujsingh026@gmail.com>
2025-06-18 11:39:24 +0530

    created Login page ui

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
         <img onClick={() => setShowSearch(true)} src={assets.search_icon} alt="search" className='w-5 cursor-pointer' />
 
         <div className='group relative'>
-            <img src={assets.profile_icon} alt="user" className='w-5 cursor-pointer' />
+            <Link to='/login'><img src={assets.profile_icon} alt="user" className='w-5 cursor-pointer' /></Link>
             <div className='group-hover:block hidden absolute drop-down-menu right-0 pt-4'>
                 <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                     <p className='cursor-pointer hover:text-black'>Profile</p>
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginPage = () => {
+
+  const [currentState, setCurrectState] = useState('Login');
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={onSubmitHandler}className='flex flex-col w-[90%] sm:max-w-96 m-auto mt-14 gap-14 text-gray-800'>
+      <div  className='inline-flex items-center gap-2 mb-2 mt-10'>
+        <p className='prata-regular text-3xl'>{currentState}</p>
+        <hr className='border-none h-[1.5px] w-8 bg-gray-800 ' />
+      </div>
+      {currentState === 'Login' ? '' : <input type='text' className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required />}
+      <input type='email' className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required />
+      <input type='password' className='w-full px-3 py-2 border border-gray-800' placeholder='Password' required />
+      <div className='w-full flex justify-between text-sm mt-[-8px]'>
+        <p className='cursor-pointer'>Forgot password?</p>
+        {
+          currentState === 'Login'
+          ? <p onClick={() => setCurrectState('Sign Up')} className='cursor-pointer'>Create an Account</p>
+          : <p onClick={() => setCurrectState('Login')} className='cursor-pointer'>Login Here</p>
+        }
+      </div>
+      <button className='bg-black text-white font-light px-8 py-2 mt-4'> { currentState === 'Login' ? 'Sign in' :
+      'Sign up' } </button>
+    </form>
   )
 }
 
