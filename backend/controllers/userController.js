13c189379bc427dd3a9e986ff235907e7193c64d
Ambuj Singh <ambujsingh026@gmail.com>
2025-06-21 16:03:40 +0530

    updated user login controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,7 +10,27 @@ const createToken = (id) => {
 
 // Route for User Login
 const loginUser = async (req,res) => {
-    
+    try{
+        const {email, password} = req.body;
+
+        const user = await userModel.findOne({email});
+
+        if (!user) {
+            return res.json({success:false, message:"User doesn't exists."})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (isMatch) {
+            const token = createToken(user._id)
+            res.json({success:true, token})
+        } else {
+            res.json({success:false, message: "Incorrect Credentials"})
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
 }
 
 
