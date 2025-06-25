bdcd65baa201558fde2ab838b88a642c74096465
Ambuj Singh <ambujsingh026@gmail.com>
2025-06-25 12:45:23 +0530

    updated ShopContext to fetch products from backend

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,7 +1,8 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, use, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const ShopContext = createContext();
 
@@ -9,9 +10,11 @@ const ShopContextProvider = (props) => {
     
     const currency = '$';
     const deliveryfee = 10;
+    const backendURL = import.meta.env.VITE_BACKEND_URL;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
+    const [products, setProducts] = useState([]);
     const navigate = useNavigate();
 
     const addToCart = async (itemId, size) => {
@@ -73,8 +76,27 @@ const ShopContextProvider = (props) => {
         return totalAmount;
     }
 
+    const fetchProducts = async () => {
+        try {
+            const response = await axios.get(backendURL + '/api/product/list');
+            if (response.data.success) {
+                setProducts(response.data.products);
+            } else {
+                toast.error(response.data.message);
+            }
+        }
+        catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    }
+    
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
     const value = {
-        products, currency, deliveryfee, search, setSearch, showSearch, setShowSearch, cartItems, addToCart, getCartCount, updateQuantity, getCartAmount, navigate
+        products, currency, deliveryfee, search, setSearch, showSearch, setShowSearch, cartItems, addToCart, getCartCount, updateQuantity, getCartAmount, navigate, backendURL
     };
 
     return (
